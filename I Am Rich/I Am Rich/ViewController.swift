f9d47bbf3a1a0891d3df00e6776afca8ddd880e5
Ryan Moye <32889189+ryanmoye16@users.noreply.github.com>
2018-10-21 18:55:01 -0400

    Add files via upload

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Ryan Moye on 10/9/18.
+//  Copyright © 2018 Ryan Moye. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
